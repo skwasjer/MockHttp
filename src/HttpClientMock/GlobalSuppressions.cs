ac3ebea42cd76a87a14e7db644f5446fdcb2853e
skwas <skwas@skwas.net>
2019-06-09 05:21:01 +0200

    Fix suppression

--- src/HttpClientMock/GlobalSuppressions.cs
+++ src/HttpClientMock/GlobalSuppressions.cs
@@ -4,5 +4,5 @@
 // Project-level suppressions either have no target or are given 
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1054:Uri parameters should not be strings", Justification = "By design: uri's are glob patterns, not matching strict URI format.", Scope = "member", Target = "~M:HttpClientMock.RequestMatchingExtensions.Url(HttpClientMock.RequestMatching,System.String)~HttpClientMock.RequestMatching")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1054:Uri parameters should not be strings", Justification = "<Pending>", Scope = "member", Target = "~M:HttpClientMock.HttpRequestMatchers.UrlMatcher.#ctor(System.String)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1054:Uri parameters should not be strings", Justification = "By design: URIs are glob patterns, not matching strict URI format.", Scope = "member", Target = "~M:HttpClientMock.RequestMatchingExtensions.Url(HttpClientMock.RequestMatching,System.String)~HttpClientMock.RequestMatching")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1054:Uri parameters should not be strings", Justification = "By design: URIs are glob patterns, not matching strict URI format.", Scope = "member", Target = "~M:HttpClientMock.Matchers.UrlMatcher.#ctor(System.String)")]
