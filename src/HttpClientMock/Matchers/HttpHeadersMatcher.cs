a28f9183f903b61d03e365387bd10e9cd337f93d
skwas <skwas@skwas.net>
2019-06-10 15:25:55 +0200

    Subclass HttpHeaders for better header parsing support

--- src/HttpClientMock/Matchers/HttpHeadersMatcher.cs
+++ src/HttpClientMock/Matchers/HttpHeadersMatcher.cs
@@ -1,31 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using HttpClientMock.Utilities;
-using Microsoft.Extensions.Primitives;
+using HttpClientMock.Http;
 
 namespace HttpClientMock.Matchers
 {
 	public class HttpHeadersMatcher : IHttpRequestMatcher
 	{
-		public HttpHeadersMatcher(IEnumerable<KeyValuePair<string, StringValues>> headers)
+		public HttpHeadersMatcher(string name, string value)
 		{
-			ExpectedHeaders = headers?.ToDictionary(h => h.Key, h => h.Value) ?? throw new ArgumentNullException(nameof(headers));
+			ExpectedHeaders = new HttpHeadersCollection
+			{
+				{ name, value }
+			};
+		}
+		public HttpHeadersMatcher(string name, IEnumerable<string> values)
+		{
+			ExpectedHeaders = new HttpHeadersCollection
+			{
+				{ name, values }
+			};
 		}
 
-		public HttpHeadersMatcher(IEnumerable<KeyValuePair<string, string[]>> headers)
-			: this(headers?.ToDictionary(h => h.Key, h => new StringValues(h.Value)))
+		public HttpHeadersMatcher(IEnumerable<KeyValuePair<string, IEnumerable<string>>> headers)
 		{
+			ExpectedHeaders = new HttpHeadersCollection();
+			foreach (KeyValuePair<string, IEnumerable<string>> header in headers)
+			{
+				ExpectedHeaders.Add(header.Key, header.Value);
+			}
 		}
 
 		public HttpHeadersMatcher(IEnumerable<KeyValuePair<string, string>> headers)
-			: this(headers?.ToDictionary(h => h.Key, h => new StringValues(h.Value)))
+			: this(headers?.ToDictionary(h => h.Key, h => Enumerable.Repeat(h.Value, 1)))
 		{
 		}
 
-		public IDictionary<string, StringValues> ExpectedHeaders { get; }
+		public HttpHeaders ExpectedHeaders { get; }
 
 		/// <inheritdoc />
 		public bool IsMatch(HttpRequestMessage request)
@@ -33,13 +45,13 @@ namespace HttpClientMock.Matchers
 			return ExpectedHeaders.All(h => IsMatch(h, request.Headers) || IsMatch(h, request.Content?.Headers));
 		}
 
-		private static bool IsMatch(KeyValuePair<string, StringValues> expectedHeader, HttpHeaders headers)
+		private static bool IsMatch(KeyValuePair<string, IEnumerable<string>> expectedHeader, HttpHeaders headers)
 		{
 			return headers != null
 				&& headers.TryGetValues(expectedHeader.Key, out IEnumerable<string> values)
 				&& values.Any(v =>
 					expectedHeader.Value.All(
-						eh => HttpUtilities.ParseHttpHeaderValue(v).Contains(eh)
+						eh => HttpHeadersCollection.ParseHttpHeaderValue(v).Contains(eh)
 					)
 				);
 		}
