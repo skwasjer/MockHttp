eaaf0b86bae1cd74c7a2031fa16a7e6df17aa5de
skwasjer <skwas@skwas.net>
2021-11-19 01:52:39 +0100

    feat(json): add JSON adapter abstraction

---
+++ src/MockHttp.Json/IJsonAdapter.cs
@@ -0,0 +1,14 @@
+﻿namespace MockHttp.Json;
+
+/// <summary>
+/// An adapter for JSON (de)serialization and comparison.
+/// </summary>
+public interface IJsonAdapter
+{
+    /// <summary>
+    /// Serializes the specified <paramref name="value" /> to JSON.
+    /// </summary>
+    /// <param name="value">The value to serialize.</param>
+    /// <returns>The JSON representation of <paramref name="value" />.</returns>
+    string Serialize(object? value);
+}
