c409ba37fa0e02b4c88744ba33cbc9d03c9cf967
Martijn Bodeman <11424653+skwasjer@users.noreply.github.com>
2023-11-21 23:37:55 +0100

    fix(xml): do not modify `XmlSettings` supplied to `XmlBody`. (#90)

---
+++ test/MockHttp.Tests/Language/Flow/Response/LinqToXmlBodyDoesNotModifySettings.cs
@@ -0,0 +1,22 @@
+﻿using System.Xml;
+using System.Xml.Linq;
+using MockHttp.Specs;
+
+namespace MockHttp.Language.Flow.Response;
+
+public sealed class LinqToXmlBodyDoesNotModifySettings : ResponseSpec
+{
+    private static readonly XmlWriterSettings OriginalSettings = new() { CloseOutput = true, Async = false };
+
+    protected override void Given(IResponseBuilder with)
+    {
+        with.XmlBody(XDocument.Parse("<xml/>"), OriginalSettings);
+    }
+
+    protected override Task Should(HttpResponseMessage response)
+    {
+        OriginalSettings.Async.Should().BeFalse();
+        OriginalSettings.CloseOutput.Should().BeTrue();
+        return Task.CompletedTask;
+    }
+}
