f22be46c9f2518f9d8590770cb18e165cda892bd
Martijn Bodeman <11424653+skwasjer@users.noreply.github.com>
2024-02-03 17:51:50 +0100

    perf(CA1848): Use the LoggerMessage delegates (#104)

---
+++ src/MockHttp.Server/Server/Log.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace MockHttp.Server;
+
+internal static partial class Log
+{
+    internal const string LogRequestMessageTemplate = "Connection id \"{ConnectionId}\", Request id \"{RequestId}\": {Message}";
+
+#if NET6_0_OR_GREATER
+    [LoggerMessage(
+        EventId = 0,
+        Level = LogLevel.Debug,
+        Message = LogRequestMessageTemplate)]
+    private static partial void LogDebugRequestMessage(ILogger logger, string connectionId, string requestId, string message, Exception? exception);
+#else
+    private static readonly Action<ILogger, string, string, string, Exception?> LogDebugRequestMessage = LoggerMessage.Define<string, string, string>(LogLevel.Debug, new EventId(0), LogRequestMessageTemplate);
+#endif
+
+    public static void LogRequestMessage(this ILogger logger, HttpContext httpContext, string message, Exception? exception = null)
+    {
+        LogDebugRequestMessage(logger, httpContext.Connection.Id, httpContext.TraceIdentifier, message, exception);
+    }
+}
