85b2c581fd0c75f60834b25e2f003a8cb57d4164
Martijn Bodeman <11424653+skwasjer@users.noreply.github.com>
2024-09-07 03:57:40 +0200

    chore(deps): bump test dependencies (#109)
    * chore(deps): replaced Serilog with own log capturing. Although we have to maintain some extra code, Serilog is not easy to maintain with the different target frameworks/libraries and its dependencies.
    
    * chore(deps): bump test dependencies xunit, Microsoft.NET.Test.Sdk and Newtonsoft.Json

---
+++ test/MockHttp.Server.Tests/Fixtures/LoggerFactoryFixture.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace MockHttp.Fixtures;
+
+public abstract class LoggerFactoryFixture : IAsyncLifetime, IAsyncDisposable
+{
+    private readonly ServiceProvider _services;
+
+    protected LoggerFactoryFixture(Action<ILoggingBuilder>? configure = null)
+    {
+        _services = new ServiceCollection()
+            .AddLogging(builder =>
+            {
+                builder.SetMinimumLevel(LogLevel.Trace);
+
+                builder
+                    .AddDebug()
+#if NET6_0_OR_GREATER
+                    .AddSimpleConsole(opts => opts.IncludeScopes = true)
+#endif
+                    ;
+
+                configure?.Invoke(builder);
+            })
+            .BuildServiceProvider();
+
+        Factory = _services.GetRequiredService<ILoggerFactory>();
+    }
+
+    public ILoggerFactory Factory { get; }
+
+    public async ValueTask DisposeAsync()
+    {
+        await DisposeAsyncCore();
+        GC.SuppressFinalize(this);
+    }
+
+    Task IAsyncLifetime.InitializeAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    Task IAsyncLifetime.DisposeAsync()
+    {
+        return DisposeAsync().AsTask();
+    }
+
+    protected virtual async ValueTask DisposeAsyncCore()
+    {
+        await _services.DisposeAsync();
+    }
+}
