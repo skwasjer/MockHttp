98bfdcb6d1efb2580c87f2d89d39fc2bc43302bb
skwasjer <11424653+skwasjer@users.noreply.github.com>
2024-09-17 08:00:53 +0200

    refactor: consolidate `RegexPattern` and parts of `WildcardPattern` into `Pattern`.
    Also adds additional factory methods and operators and improve XML docs and coverage.

--- src/MockHttp/Patterns/IPattern.cs
+++ src/MockHttp/Patterns/IPattern.cs
@@ -1,7 +1,17 @@
 ﻿namespace MockHttp.Patterns;
 
+/// <summary>
+/// Describes a pattern that matches a string.
+/// </summary>
 internal interface IPattern
 {
+    /// <summary>
+    /// Gets the pattern value.
+    /// </summary>
     string Value { get; }
+
+    /// <summary>
+    /// Gets the pattern match delegate.
+    /// </summary>
     Func<string, bool> IsMatch { get; }
 }
