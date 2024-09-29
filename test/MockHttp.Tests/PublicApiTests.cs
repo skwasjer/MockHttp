06a37bee76ebe12c1288cf1fe82aa21bf08346a9
skwasjer <11424653+skwasjer@users.noreply.github.com>
2024-09-29 15:04:34 +0200

    test: verify public API

---
+++ test/MockHttp.Tests/PublicApiTests.cs
@@ -0,0 +1,12 @@
+﻿
+using MockHttp.Specs;
+
+namespace MockHttp;
+
+public sealed class PublicApiTests : PublicApiSpec
+{
+    public PublicApiTests()
+        : base(typeof(MockHttpHandler))
+    {
+    }
+}
