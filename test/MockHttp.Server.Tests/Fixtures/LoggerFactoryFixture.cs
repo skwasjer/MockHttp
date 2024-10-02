f5641d4c5c170bc0b71cb3aa8566e05db729661f
skwasjer <11424653+skwasjer@users.noreply.github.com>
2024-10-02 02:34:48 +0200

    test: simplify request logging

--- test/MockHttp.Server.Tests/Fixtures/LoggerFactoryFixture.cs
+++ test/MockHttp.Server.Tests/Fixtures/LoggerFactoryFixture.cs
@@ -1,50 +1,44 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Testing;
 
 namespace MockHttp.Fixtures;
 
-public abstract class LoggerFactoryFixture : IAsyncLifetime, IAsyncDisposable
+internal sealed class LoggerFactoryFixture : IAsyncDisposable, IDisposable
 {
     private readonly ServiceProvider _services;
 
-    protected LoggerFactoryFixture(Action<ILoggingBuilder>? configure = null)
+    public LoggerFactoryFixture(Action<ILoggingBuilder>? configure = null)
     {
         _services = new ServiceCollection()
-            .AddLogging(builder =>
-            {
-                builder.SetMinimumLevel(LogLevel.Trace);
+            .AddLogging(
+                builder =>
+                {
+                    builder.SetMinimumLevel(LogLevel.Trace);
 
-                builder
-                    .AddDebug()
-                    .AddSimpleConsole(opts => opts.IncludeScopes = true)
-                    ;
+                    builder
+                        .AddFakeLogging()
+                        .AddDebug();
 
-                configure?.Invoke(builder);
-            })
+                    configure?.Invoke(builder);
+                }
+            )
             .BuildServiceProvider();
 
         Factory = _services.GetRequiredService<ILoggerFactory>();
+        FakeLogCollector = _services.GetRequiredService<FakeLogCollector>();
     }
 
     public ILoggerFactory Factory { get; }
 
+    public FakeLogCollector FakeLogCollector { get; }
+
+    public void Dispose()
+    {
+        _services.Dispose();
+    }
+
     public async ValueTask DisposeAsync()
-    {
-        await DisposeAsyncCore();
-        GC.SuppressFinalize(this);
-    }
-
-    Task IAsyncLifetime.InitializeAsync()
-    {
-        return Task.CompletedTask;
-    }
-
-    Task IAsyncLifetime.DisposeAsync()
-    {
-        return DisposeAsync().AsTask();
-    }
-
-    protected virtual async ValueTask DisposeAsyncCore()
     {
         await _services.DisposeAsync();
     }
