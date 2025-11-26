e3c8465f26bd94878e99f5dba788b67e071cffb3
skwasjer <11424653+skwasjer@users.noreply.github.com>
2025-11-26 17:06:54 +0100

    chore: clean up namespaces

--- src/MockHttp.Json/SystemTextJson/RequestMatchingExtensions.cs
+++ src/MockHttp.Json/SystemTextJson/RequestMatchingExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System.Text.Json;
-using Newtonsoft.Json;
 
 namespace MockHttp.Json.SystemTextJson;
 
--- src/MockHttp.Server/Server/ExceptionHandler.cs
+++ src/MockHttp.Server/Server/ExceptionHandler.cs
@@ -1,5 +1,4 @@
-﻿using System.Net.Http;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
